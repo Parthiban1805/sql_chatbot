974076f84d7b24e72a71af4e8ed321f71f1530d0
Parthiban <parthi.s1805@gmail.com>
2025-01-27 09:55:36 +0530

    requirement.txt

---
+++ server/version_check.py
@@ -0,0 +1,9 @@
+import bcrypt
+import flask
+import psycopg2
+import google.generativeai
+
+print("bcrypt version:", bcrypt.__version__)
+print("Flask version:", flask.__version__)
+print("psycopg2 version:", psycopg2.__version__)
+print("google-generativeai version:", google.generativeai.__version__)
